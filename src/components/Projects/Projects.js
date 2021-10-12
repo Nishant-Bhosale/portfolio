659b7bb1a2a3ca8ddd305f8a34be7c9cda8bde23
Nishant Bhosale <nishantbhosale244@gmail.com>
2021-10-12 15:25:48 +0530

    Added a new Project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,55 +1,57 @@
-import React from "react";
-import ecommerceImg from "../../images/ecommerce-site.png";
-import netflixCloneImg from "../../images/netflix.png";
-import contactKeeperImg from "../../images/contact-keeper.PNG";
-import chatAppImg from "../../images/socketio.png";
-import Project from "../Project/Project";
-import HotelCard from "../../images/hotel.png";
-import TodoApp from "../../images/todo-app.png";
-import "./Projects.css";
+import React from 'react';
+import ecommerceImg from '../../images/ecommerce-site.png';
+import netflixCloneImg from '../../images/netflix.png';
+import contactKeeperImg from '../../images/contact-keeper.PNG';
+import chatAppImg from '../../images/socketio.png';
+import Project from '../Project/Project';
+import gamerDiaryImg from '../../images/theGamerDiary.jpg';
+import TodoApp from '../../images/todo-app.png';
+import './Projects.css';
 
 const Projects = () => {
 	//List of projects
 	const projects = [
 		{
-			title: "E-Commerce WebApp",
-			demoLink: "https://ecommerce-site-9d5dc7.netlify.app/",
-			sourceCodeLink: "https://github.com/Nishant-Bhosale/amazon-clone",
+			title: 'The GamerDiary',
+			demoLink: 'https://the-gamer-diary.herokuapp.com/',
+			sourceCodeLink: 'https://github.com/Nishant-Bhosale/TheGamerDiary',
+			imageSrc: gamerDiaryImg,
+		},
+		{
+			title: 'E-Commerce WebApp',
+			demoLink: 'https://ecommerce-site-9d5dc7.netlify.app/',
+			sourceCodeLink: 'https://github.com/Nishant-Bhosale/amazon-clone',
 			imageSrc: ecommerceImg,
 		},
 		{
-			title: "Netflix-Clone",
-			demoLink: "http://netflix-movie-app-clone.herokuapp.com/",
-			sourceCodeLink: "https://github.com/Nishant-Bhosale/MovieApp",
+			title: 'Netflix-Clone',
+			demoLink: 'http://netflix-movie-app-clone.herokuapp.com/',
+			sourceCodeLink: 'https://github.com/Nishant-Bhosale/MovieApp',
 			imageSrc: netflixCloneImg,
 		},
 		{
-			title: "Contact-Keeper",
-			demoLink: "http://contact-keeper7.herokuapp.com/",
-			sourceCodeLink: "https://github.com/Nishant-Bhosale/ContackKeeper",
+			title: 'Contact-Keeper',
+			demoLink: 'http://contact-keeper7.herokuapp.com/',
+			sourceCodeLink: 'https://github.com/Nishant-Bhosale/ContackKeeper',
 			imageSrc: contactKeeperImg,
 		},
+
 		{
-			title: "Chat-App",
-			demoLink: "https://evening-badlands-19902.herokuapp.com/",
-			sourceCodeLink: "https://github.com/Nishant-Bhosale/Chat-App",
+			title: 'Chat-App',
+			demoLink: 'https://evening-badlands-19902.herokuapp.com/',
+			sourceCodeLink: 'https://github.com/Nishant-Bhosale/Chat-App',
 			imageSrc: chatAppImg,
 		},
 		{
-			title: "Hotel Menu Card",
-			sourceCodeLink: "https://github.com/Nishant-Bhosale/HotelMenuCard",
-			imageSrc: HotelCard,
-		},
-		{
-			title: "Todo-List with TypeScript",
+			title: 'Todo-List with TypeScript',
 			sourceCodeLink:
-				"https://github.com/Nishant-Bhosale/TodoAppWithTypescript",
+				'https://github.com/Nishant-Bhosale/TodoAppWithTypescript',
 			imageSrc: TodoApp,
 		},
 	];
 
 	return (
-		<div style={{ background: "rgb(40, 40, 40)" }}>
+		<div style={{ background: 'rgb(40, 40, 40)' }}>
 			<h1 className="work-heading">My Work</h1>
 			<div id="work" className="projects-container" projects={projects}>
 				{projects.map((project, index) => {
