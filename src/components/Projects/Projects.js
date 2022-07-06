2ba836a5fa2a2709e506ff415bc98eec0c28fca5
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-07-06 18:25:25 +0530

    Changed project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,57 +1,56 @@
-import React from 'react';
-import ecommerceImg from '../../images/ecommerce-site.png';
-import netflixCloneImg from '../../images/netflix.png';
-import contactKeeperImg from '../../images/contact-keeper.PNG';
-import chatAppImg from '../../images/socketio.png';
-import Project from '../Project/Project';
-import gamerDiaryImg from '../../images/theGamerDiary.jpg';
-import PingPong from '../../images/ping-pong.PNG';
-import './Projects.css';
+import React from "react";
+import ecommerceImg from "../../images/ecommerce-site.png";
+import netflixCloneImg from "../../images/netflix.png";
+import contactKeeperImg from "../../images/contact-keeper.PNG";
+import chatAppImg from "../../images/socketio.png";
+import Project from "../Project/Project";
+import blogImage from "../../images/blog-image.PNG";
+import PingPong from "../../images/ping-pong.PNG";
+import "./Projects.css";
 
 const Projects = () => {
 	//List of projects
 	const projects = [
 		{
-			title: 'The GamerDiary',
-			demoLink: 'https://the-gamer-diary.herokuapp.com/',
-			sourceCodeLink: 'https://github.com/Nishant-Bhosale/TheGamerDiary',
-			imageSrc: gamerDiaryImg,
+			title: "Personal Blog",
+			demoLink: "https://nishantbhosale.vercel.app/",
+			sourceCodeLink: "https://github.com/Nishant-Bhosale/Personal-Blog",
+			imageSrc: blogImage,
 		},
 		{
-			title: 'E-Commerce WebApp',
-			demoLink: 'https://ecommerce-site-9d5dc7.netlify.app/',
-			sourceCodeLink: 'https://github.com/Nishant-Bhosale/amazon-clone',
+			title: "E-Commerce WebApp",
+			demoLink: "https://ecommerce-site-9d5dc7.netlify.app/",
+			sourceCodeLink: "https://github.com/Nishant-Bhosale/amazon-clone",
 			imageSrc: ecommerceImg,
 		},
 		{
-			title: 'Netflix-Clone',
-			demoLink: 'http://netflix-movie-app-clone.herokuapp.com/',
-			sourceCodeLink: 'https://github.com/Nishant-Bhosale/MovieApp',
+			title: "Netflix-Clone",
+			demoLink: "http://netflix-movie-app-clone.herokuapp.com/",
+			sourceCodeLink: "https://github.com/Nishant-Bhosale/MovieApp",
 			imageSrc: netflixCloneImg,
 		},
 		{
-			title: 'Contact-Keeper',
-			demoLink: 'http://contact-keeper7.herokuapp.com/',
-			sourceCodeLink: 'https://github.com/Nishant-Bhosale/ContackKeeper',
+			title: "Contact-Keeper",
+			demoLink: "http://contact-keeper7.herokuapp.com/",
+			sourceCodeLink: "https://github.com/Nishant-Bhosale/ContackKeeper",
 			imageSrc: contactKeeperImg,
 		},
 		{
-			title: 'Chat-App',
-			demoLink: 'https://evening-badlands-19902.herokuapp.com/',
-			sourceCodeLink: 'https://github.com/Nishant-Bhosale/Chat-App',
+			title: "Chat-App",
+			demoLink: "https://evening-badlands-19902.herokuapp.com/",
+			sourceCodeLink: "https://github.com/Nishant-Bhosale/Chat-App",
 			imageSrc: chatAppImg,
 		},
 		{
-			title: 'Ping-Pong Game',
-			demoLink: 'https://ping-pong-yeyaaaa.netlify.app/',
-			sourceCodeLink:
-				'https://github.com/Nishant-Bhosale/Ping-Pong_Game',
+			title: "Ping-Pong Game",
+			demoLink: "https://ping-pong-yeyaaaa.netlify.app/",
+			sourceCodeLink: "https://github.com/Nishant-Bhosale/Ping-Pong_Game",
 			imageSrc: PingPong,
 		},
 	];
 
 	return (
-		<div style={{ background: 'rgb(40, 40, 40)' }}>
+		<div style={{ background: "rgb(40, 40, 40)" }}>
 			<h1 className="work-heading">My Work</h1>
 			<div id="work" className="projects-container" projects={projects}>
 				{projects.map((project, index) => {
